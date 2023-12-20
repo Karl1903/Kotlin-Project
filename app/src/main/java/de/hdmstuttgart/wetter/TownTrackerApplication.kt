ebbb24a608fda47f684056d05b1d07d0d45c0473
Karl1903 <the.tritones.info@gmail.com>
2023-12-20 12:08:19 +0100

    what

--- app/src/main/java/de/hdmstuttgart/wetter/TownTrackerApplication.kt
+++ app/src/main/java/de/hdmstuttgart/wetter/TownTrackerApplication.kt
@@ -10,18 +10,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 class TownTrackerApplication : Application() {
     private val database by lazy { TownDatabase.getDatabase(this) }
     val repository by lazy { TownRepository(database.townDao()) }
+    companion object {
 
-    //todo: wetter api.
-    private val retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        //Retrofit Setup to do API-Calls to the Weather-API openweathermap.org..
+        private val retrofit by lazy {
+
+
+            Retrofit.Builder()
+                .baseUrl(Configuration.BASE_LINK)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+
     }
-
-    //Wetter api key: cc5ef9e3576dc1e8bc30087dae5ee9ca
-    //The endpoint: https://api.openweathermap.org.
-    //London: https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cc5ef9e3576dc1e8bc30087dae5ee9ca
-
     val weatherApi: WeatherApi by lazy { retrofit.create(WeatherApi::class.java) }
 }
